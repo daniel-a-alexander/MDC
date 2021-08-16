192ab41865aaeed3d4cafe0e8999a0462ad793a5
Daniel A. Alexander <42974485+daniel-a-alexander@users.noreply.github.com>
2021-08-16 18:09:46 -0400

    Add files via upload

---
+++ meandistconf.m
@@ -0,0 +1,60 @@
+function mdc = meandistconf(A, B)
+
+%{
+by Daniel Alexander, 2021
+
+INPUTS:
+
+A,B     - binary masks, each size mxn
+
+OUTPUTS:
+
+mdc     - mean distance to conformity metric, scalar
+
+%}
+
+    if ~isequal(size(A), size(B))
+        disp('Error: arrays must have equal size')
+        return
+    else
+        % convert arrays to logical
+        A = logical(A);
+        B = logical(B);
+        
+        % Initialize set arrays
+        C1 = zeros(size(A));
+        C2 = zeros(size(B));
+        D = zeros(size(A));
+        
+        % Determine disagreement sets
+        C1(A~=0 & A~=B) = 1; % all points in A that are not in B
+        C2(B~=0 & B~=A) = 1; % all points in B that are not in A
+        C = C1 + C2; % sum
+
+        % Determine agreement sets
+        D(A==B & A~=0) = 1; % all points in A and B
+
+        % Find x and y coordinates 
+        [r1,c1] = find(C); % (y,x) for all disagreement points
+        [r2,c2] = find(D); % (y,x) for all agreement points
+
+        % Create arrays of data points (agreement) and query points
+        % (disagreement)
+        P = [r2, c2]; % data points
+        PQ = [r1, c1]; % query points
+        
+        % Calculate MDC
+        dists = zeros(size(PQ,1),1); % initialize array of distances for each query point
+        for i=1:size(PQ,1)
+            temp = sqrt(sum((P-PQ(i,:)).^2,2));
+            dists(i) = min(temp(:));
+        end
+        mdc = mean(dists(:));
+     
+    end
+end
+
+%{
+note, I tried to use dsearchn instead of this loop, but it was slower. :(
+%}
+   
